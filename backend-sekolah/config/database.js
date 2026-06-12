8802ed3c788c5327eca2939d3c800ad00cf2e1ac
Rifai <email_andirifai23456@gmail.com>
2026-06-12 17:44:08 +0800

    fix: database config use env variables

--- backend-sekolah/config/database.js
+++ backend-sekolah/config/database.js
@@ -1,5 +1,4 @@
-// config/database.js
-const { Sequelize } = require('sequelize');
+﻿const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'siakad_sekolah_orm',
@@ -7,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false
   }
